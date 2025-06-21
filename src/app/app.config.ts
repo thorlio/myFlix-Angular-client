ce257f821e91033416d661476697385395c85602
thorlio <thorlio3@gmail.com>
2025-06-21 01:39:14 -0700

    initial commit for myFlix-Angular-client API service

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,11 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig } from '@angular/core';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideBrowserGlobalErrorListeners(),
-    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideHttpClient(withInterceptors([])), // Enables HttpClient
     provideRouter(routes)
   ]
 };
